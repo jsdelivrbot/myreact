12aa2b5fd5d0b2fad87dbfb2289b9b351d447b97
maryamshoraka <maryam.shoraka@yahoo.com>
2017-01-21 13:15:18 +1100

    Add SearchBar component instance

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+import SearchBar from './components/search_bar';
+
 // declare variable to hold API key
 const API_KEY='AIzaSyCDGK152IISgfnjEZrT6x4IhHeNkDrJObY';
 
 // Create a new component. This component should produce
 // some HTML
 const App =()=>{ // we use '=()=>{}'  instead of funsction(){}
-    return <div>Hi!!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+        );
 }
 
 // Take This component's generated HTML and put is
