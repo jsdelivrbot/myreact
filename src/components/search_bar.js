36eb988ee719822e765b41370001c997b0668813
maryamshoraka <maryam.shoraka@yahoo.com>
2017-01-22 10:28:54 +1100

    Editet term in state declaration

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component{
     constructor(props){
         super(props);
 
-        this.state = {term=''};
+        this.state = {term:''};
     }
     render(){
         return <input onChange={event=> console.log(event.target.value)}/>;
