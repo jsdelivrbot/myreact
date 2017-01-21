90e4a441dfa5daa1ed150516b05013f8d3065102
maryamshoraka <maryam.shoraka@yahoo.com>
2017-01-21 13:46:27 +1100

    Change functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-
+/* Function component
 const SearchBar =()=>{
     return <input />;
 };
+*/
 
+//class component by extends React.Component we give it all accessibilities to what react component has
+
+class SearchBar extends React.Component{
+    render(){
+        return <input />;
+    }
+}
 
 export default SearchBar;
 
