32bd1e83695dc05d8ee2020e865d7bab834c1eba
maryamshoraka <maryam.shoraka@yahoo.com>
2017-01-22 10:03:22 +1100

    Adding state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,6 +9,11 @@ const SearchBar =()=>{
 //class component by extends React.Component we give it all accessibilities to what react component has
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term=''};
+    }
     render(){
         return <input onChange={event=> console.log(event.target.value)}/>;
     }
