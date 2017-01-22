48faa82ae20af23c66e9c527cc94d541da67e95b
maryamshoraka <maryam.shoraka@yahoo.com>
2017-01-22 12:17:43 +1100

    Adding search functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 // declare variable to hold API key
 const API_KEY='AIzaSyCDGK152IISgfnjEZrT6x4IhHeNkDrJObY';
 
+YTSearch({key:API_KEY, term:'surfboard'}, function(data){
+    console.log(data);
+});
+
 // Create a new component. This component should produce
 // some HTML
-const App =()=>{ // we use '=()=>{}'  instead of funsction(){}
-    return (
-        <div>
-            <SearchBar />
-        </div>
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
         );
+    }
 }
 
 // Take This component's generated HTML and put is
