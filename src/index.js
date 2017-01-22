609130d873cedf5ca696c2016d3d71b8004f389c
maryamshoraka <maryam.shoraka@yahoo.com>
2017-01-22 13:33:01 +1100

    Added video_list & updated index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 // declare variable to hold API key
 const API_KEY='AIzaSyCDGK152IISgfnjEZrT6x4IhHeNkDrJObY';
@@ -24,6 +25,7 @@ class App extends Component{
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
